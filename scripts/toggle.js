f5812508f9c34eedac91c0a797c89034a90cd909
Black Myagi <blackmyagi@proton.me>
2023-08-21 15:45:57 +0100

    modified

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -15,6 +15,36 @@ function toggleMenu() {
       open.style.display = 'none';
     }
   }
+
+  // make a x seconds count down timer
+  function countDownTimer() {
+    var countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
+    var x = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      // Time calculations for days, hours, minutes and seconds
+      // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      // Display the result in the element with id="demo"
+      document.getElementById("countdown").innerHTML = seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  }
+    
+ 
+  
+
+  
+
+
+
   
   window.addEventListener('resize', (e) => {
     var x = document.querySelector('nav>ul');
